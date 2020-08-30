44a97c0aeaf9797f2a3b267a94f5265abeaabaf2
Дмитрий Трофимов <d.trofimov@kt-team.de>
2020-08-30 22:24:48 +0400

    feature : icon компонент, для рендеринга feather-icon по названию иконки

---
+++ src/components/ui/AppIcon.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+import * as icons from 'vue-feather-icons';
+
+export default Vue.extend({
+  functional: true,
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+    },
+  },
+  render(h, ctx) {
+    const featherIcons: Record<string, any> = icons;
+    const { staticClass, attrs } = ctx.data;
+    const { icon, ...props } = ctx.props;
+    return h(featherIcons[icon], {
+      props,
+      attrs,
+      class: {
+        ...(staticClass && {
+          [staticClass]: true,
+        }),
+      },
+    });
+  },
+});
+</script>
