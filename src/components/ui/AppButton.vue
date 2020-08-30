3cb1d1d7f6a9318f4fb1999cb8e4e3250c4593ce
Дмитрий Трофимов <d.trofimov@kt-team.de>
2020-08-30 22:25:46 +0400

    feature : button компонент

---
+++ src/components/ui/AppButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="text-white bg-blue-500 hover:bg-blue-700 text-sm px-5 py-2 rounded-md">
+    <slot>
+      Button
+    </slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class AppButton extends Vue {}
+</script>
+
+<style scoped>
+
+</style>
